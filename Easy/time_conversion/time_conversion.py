5b5624a483498fc173f1076fc4eac7dc49982f99
Neel Kotak <neelkotak13@gmail.com>
2020-08-10 07:56:47 -0500

    solved time_conversion, needed to add in cases for 12AM and 12PM

--- Easy/time_conversion/time_conversion.py
+++ Easy/time_conversion/time_conversion.py
@@ -7,9 +7,10 @@ output: convert and print the given time in 24 hr format
 
 solution:
     1. split the input string into hh, mm, ss, and AM/PM
-    2. if input is AM, return same output, but remove the PM
+    2. if input is AM, return same output, but remove the PM, if value is 12,
+       replace with 00
     3. if input is PM, add 12 to hh and return the modified hh w/same mm and ss
-       but remove the PM
+       but remove the PM, do not add 12, if value is not 12
 '''
 def time_conversion(s):
     # add colon in front of AM/PM to use as delimeter
@@ -20,9 +21,15 @@ def time_conversion(s):
     meridian = s[len(s)-1]
     # if AM, no change, return same value, except omit AM
     if meridian == 'AM':
+        # need to change 12 to 00
+        if s[0] == '12':
+            s[0] = '00'
         return ':'.join(s[:len(s)-1])
     # if PM, add 12 to hh and then return value omitting PM
     else:
+        # need to leave 12 the same
+        if s[0] == '12':
+            return ':'.join(s[:len(s)-1]) 
         new_val = int(s[0]) + 12
         s[0] = str(new_val)
         return ':'.join(s[:len(s)-1]) 
